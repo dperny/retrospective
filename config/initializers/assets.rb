014fa3fea27f6e2af885801601ab021cbdb183d2
Drew Erny <drewerny@gmail.com>
2015-03-27 02:42:56 +0000

    fixed more breaks

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( map.js )
 Rails.application.config.assets.precompile += %w(soundmanager2.swf soundmanager2_flash9.swf)
+Rails.application.config.assets.paths << Rails.root.join("vendor","assets","images","360player")
+Rails.application.config.assets.precompile += %w( *.jpg *.png *.gif *.jpeg)
